8aa7bf421692a657e69f736bdbc1b0f599e8e99c
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-05-16 16:45:40 +0530

    final

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.post("/home",function(req,res){
   res.redirect("/")
 });
 
-app.listen(process.env.port||3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("Server is started at port 3000");
 });
